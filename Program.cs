9a47227fa4c8dddf4030a0b241679b1ff66b83b5
chandankkrr <chandankkrr@gmail.com>
2019-12-12 21:42:15 +1100

    Initial commit. Trying out Twilio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace twilioExplore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TwilioClient.Init(
+                "accountSid",
+                "authToken"
+            );
+
+            MessageResource.Create(
+                to: new PhoneNumber("XXXX XXX XXX"),
+                from: new PhoneNumber("XXXX XXX XXX"),
+                body: "Hello from c#"
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
